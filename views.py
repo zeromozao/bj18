471cff747d91f33be38059a29892ca109a561726
zeromozao <847348419@qq.com>
2021-08-03 16:25:44 +0800

    '创建视图login'

--- views.py
+++ views.py
@@ -1,5 +1,9 @@
 # -*- coding: UTF-8 –*-
 from django.http import HttpResponse
+from django.shortcuts import redirect
 
 def index(request):
     return HttpResponse('index')
+
+def login(request):
+    return redirect('/index')
